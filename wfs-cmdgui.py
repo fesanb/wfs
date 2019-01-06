4f01b0438dd7ddf2664b6b124c626add8a116a3d
Stefan Bahrawy <stefan@bahrawwy.net>
2019-01-06 21:24:19 +0100

    update

--- wfs-cmdgui.py
+++ wfs-cmdgui.py
@@ -7,11 +7,12 @@ import curses
 from curses import wrapper
 import mysql.connector
 
-a = 0
-
 
 def main(stdscr):
     stdscr.clear()
+
+    a = 0
+    r = 0
 
     cnx = mysql.connector.connect(user='wfs', database='wfs', password='wfs22')
     cursor = cnx.cursor()
@@ -20,7 +21,19 @@ def main(stdscr):
     cursor.execute(get_wind)
     wind = cursor.fetchone()
 
-    stdscr.addstr(str(wind[0]) + str(wind[1]) + str(wind[2]))
+    #stdscr.addstr(str(wind[0]) + str(wind[1]) + str(wind[2]))
+    while a < 10:
+        a += 1
+        stdscr.addstr(2, 10, str(a))
+        if r == 1:
+            stdscr.addstr(2, 8, "/")
+            r = 0
+        else:
+            stdscr.addstr(2, 8, "\\")
+            r = 1
+
+        time.sleep(0.5)
+        stdscr.refresh()
 
     stdscr.refresh()
     stdscr.getkey()
