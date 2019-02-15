c07c8df00273dfab2885bce7e0e25334c618b26f
Stefan Bahrawy <stefan@bahrawwy.net>
2019-02-15 18:11:21 +0100

    GUI almost complete with data output. Missing a few labels to clear output for user.

--- bak.py
+++ bak.py
@@ -1,72 +1,103 @@
-def mysql_fetch(self):
-    while True:
-        cnx = mysql.connector.connect(user='wfs', database='wfs', password='wfs22')
-        cursor = cnx.cursor()
+# -*- coding: utf-8 -*-
+"""
+This example demonstrates many of the 2D plotting capabilities
+in pyqtgraph. All of the plots may be panned/scaled by dragging with 
+the left/right mouse buttons. Right click on any plot to show a context menu.
+"""
 
-        get_wind = "SELECT * FROM wind WHERE id=(SELECT MAX(id) FROM wind)"
-        cursor.execute(get_wind)
-        db_wind = cursor.fetchone()
-        self.wind = str(db_wind[1])
-        self.wind_timestamp = str(db_wind[2])
+#import initExample ## Add path to library (just for examples; you do not need this)
 
-        get_sens = "SELECT * FROM sens WHERE id=(SELECT MAX(id) FROM sens)"
-        cursor.execute(get_sens)
-        db_sens = cursor.fetchone()
-        self.temp = str(db_sens[1])
-        self.hum = str(round(db_sens[2]))
-        self.atp = str(db_sens[3])
-        self.sens_timestamp = str(db_sens[4])
 
-        get_gps = "SELECT * FROM gps WHERE id=(SELECT MAX(id) FROM gps)"
-        cursor.execute(get_gps)
-        db_gps = cursor.fetchone()
-        self.lat = str(db_gps[1])
-        self.long = str(db_gps[2])
-        self.alt = str(db_gps[3])
-        self.gps_timestamp = str(db_gps[4])
+from pyqtgraph.Qt import QtGui, QtCore
+import numpy as np
+import pyqtgraph as pg
 
-        # TEMP
-        get_max_wind_12 = "SELECT MAX(wind) FROM wind  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 12 HOUR)"
-        get_max_wind_24 = "SELECT MAX(wind) FROM wind  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 24 HOUR)"
-        get_max_wind = "SELECT MAX(wind) FROM wind"
+#QtGui.QApplication.setGraphicsSystem('raster')
+app = QtGui.QApplication([])
+#mw = QtGui.QMainWindow()
+#mw.resize(800,800)
 
-        get_min_wind_12 = "SELECT MIN(wind) FROM wind  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 12 HOUR)"
-        get_min_wind_24 = "SELECT MIN(wind) FROM wind  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 24 HOUR)"
+win = pg.GraphicsWindow(title="Basic plotting examples")
+win.resize(1000,600)
+win.setWindowTitle('pyqtgraph example: Plotting')
 
-        get_mean_wind = "SELECT AVG(wind) FROM wind  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 10 MINUTE)"
+# Enable antialiasing for prettier plots
+pg.setConfigOptions(antialias=True)
 
-        # TEMP
-        get_max_temp_12 = "SELECT MAX(temp) FROM sens  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 12 HOUR)"
-        get_max_temp_24 = "SELECT MAX(temp) FROM sens  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 24 HOUR)"
-        get_max_temp = "SELECT MAX(wind) FROM sens"
+p1 = win.addPlot(title="Basic array plotting", y=np.random.normal(size=100))
 
-        get_min_temp_12 = "SELECT MIN(temp) FROM sens  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 12 HOUR)"
-        get_min_temp_24 = "SELECT MIN(temp) FROM sens  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 24 HOUR)"
-        get_min_temp = "SELECT MIN(temp) FROM sens"
+p2 = win.addPlot(title="Multiple curves")
+p2.plot(np.random.normal(size=100), pen=(255,0,0), name="Red curve")
+p2.plot(np.random.normal(size=110)+5, pen=(0,255,0), name="Green curve")
+p2.plot(np.random.normal(size=120)+10, pen=(0,0,255), name="Blue curve")
 
-        # HUM
-        get_max_hum_12 = "SELECT MAX(hum) FROM sens  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 12 HOUR)"
-        get_max_hum_24 = "SELECT MAX(hum) FROM sens  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 24 HOUR)"
-        get_max_hum = "SELECT MAX(hum) FROM sens"
+p3 = win.addPlot(title="Drawing with points")
+p3.plot(np.random.normal(size=100), pen=(200,200,200), symbolBrush=(255,0,0), symbolPen='w')
 
-        get_min_hum_12 = "SELECT MIN(hum) FROM sens  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 12 HOUR)"
-        get_min_hum_24 = "SELECT MIN(hum) FROM sens  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 24 HOUR)"
-        get_min_hum = "SELECT MIN(hum) FROM sens"
 
-        # ATP
-        get_max_atp_12 = "SELECT MAX(atp) FROM sens  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 12 HOUR)"
-        get_max_atp_24 = "SELECT MAX(atp) FROM sens  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 24 HOUR)"
-        get_max_atp = "SELECT MAX(atp) FROM sens"
+win.nextRow()
 
-        get_min_atp_12 = "SELECT MIN(atp) FROM sens  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 12 HOUR)"
-        get_min_atp_24 = "SELECT MIN(atp) FROM sens  WHERE tmestmp >= DATE_SUB(NOW(), INTERVAL 24 HOUR)"
-        get_min_atp = "SELECT MIN(atp) FROM sens"
+p4 = win.addPlot(title="Parametric, grid enabled")
+x = np.cos(np.linspace(0, 2*np.pi, 1000))
+y = np.sin(np.linspace(0, 4*np.pi, 1000))
+p4.plot(x, y)
+p4.showGrid(x=True, y=True)
 
-        cursor.execute(get_mean_wind)
-        db_mean_wind = cursor.fetchone()
-        try:
-            self.meanwind = str(round(db_mean_wind[0], 1))
-        except:
-            self.meanwind = "0"
+p5 = win.addPlot(title="Scatter plot, axis labels, log scale")
+x = np.random.normal(size=1000) * 1e-5
+y = x*1000 + 0.005 * np.random.normal(size=1000)
+y -= y.min()-1.0
+mask = x > 1e-15
+x = x[mask]
+y = y[mask]
+p5.plot(x, y, pen=None, symbol='t', symbolPen=None, symbolSize=10, symbolBrush=(100, 100, 255, 50))
+p5.setLabel('left', "Y Axis", units='A')
+p5.setLabel('bottom', "Y Axis", units='s')
+p5.setLogMode(x=True, y=False)
 
-        # print("Thread Running")
+p6 = win.addPlot(title="Updating plot")
+curve = p6.plot(pen='y')
+data = np.random.normal(size=(10,1000))
+ptr = 0
+def update():
+    global curve, data, ptr, p6
+    curve.setData(data[ptr%10])
+    if ptr == 0:
+        p6.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
+    ptr += 1
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(50)
+
+
+win.nextRow()
+
+p7 = win.addPlot(title="Filled plot, axis disabled")
+y = np.sin(np.linspace(0, 10, 1000)) + np.random.normal(size=1000, scale=0.1)
+p7.plot(y, fillLevel=-0.3, brush=(50,50,200,100))
+p7.showAxis('bottom', False)
+
+
+x2 = np.linspace(-100, 100, 1000)
+data2 = np.sin(x2) / x2
+p8 = win.addPlot(title="Region Selection")
+p8.plot(data2, pen=(255,255,255,200))
+lr = pg.LinearRegionItem([400,700])
+lr.setZValue(-10)
+p8.addItem(lr)
+
+p9 = win.addPlot(title="Zoom on selected region")
+p9.plot(data2)
+def updatePlot():
+    p9.setXRange(*lr.getRegion(), padding=0)
+def updateRegion():
+    lr.setRegion(p9.getViewBox().viewRange()[0])
+lr.sigRegionChanged.connect(updatePlot)
+p9.sigXRangeChanged.connect(updateRegion)
+updatePlot()
+
+## Start Qt event loop unless running in interactive mode or using pyside.
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
--- testing.py
+++ testing.py
@@ -1,29 +1,5 @@
-import sys
-import mysql.connector
-import threading
-import time
+a = "None"
+b = None
 
-
-get_wind = "SELECT * FROM wind WHERE id=(SELECT MAX(id) FROM wind)"
-
-
-class GetData(object):
-    def __init__(self, interval=1):
-        self.interval = interval
-
-        thread = threading.Thread(target=self.mysql_fetch, args=())
-        thread.daemon = True
-        thread.start()
-
-        self.query = query
-
-    def mysql_fetch(self, query):
-        cnx = mysql.connector.connect(user='wfs', database='wfs', password='wfs22')
-        cursor = cnx.cursor()
-        cursor.execute(query)
-        result = cursor.fetchone()
-        time.sleep(self.interval)
-        return result
-
-
-print(GetData.mysql_fetch(get_wind))
+if b is None:
+    print("yes")
