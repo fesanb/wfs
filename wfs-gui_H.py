7b0c35a9d38e09759c905a9083b1108b5b7a78be
fesanb <stefan@bahrawy.net>
2019-06-10 11:50:11 +0200

    Final touches for Beito

--- wfs-gui_H.py
+++ wfs-gui_H.py
@@ -459,52 +459,53 @@ class App(QWidget):
         self.sensorG.addWidget(QLabel("Hum: "), 2, 0)
         self.sensorG.addWidget(QLabel("ATP: "), 3, 0)
 
-        self.sensdataT = QLabel(fetch_sens.temp)
-        # self.sensdataT.setAlignment(Qt.AlignHCenter)
-        self.sensorG.addWidget(self.sensdataT, 1, 1)
-
-        self.sensdataH = QLabel(fetch_sens.hum)
-        self.sensorG.addWidget(self.sensdataH, 2, 1)
-
-        self.sensdataA = QLabel(fetch_sens.atp)
-        self.sensorG.addWidget(self.sensdataA, 3, 1)
-
         try:
+            self.sensdataT = QLabel(fetch_sens.temp)
+            # self.sensdataT.setAlignment(Qt.AlignHCenter)
+            self.sensorG.addWidget(self.sensdataT, 1, 1)
+
+            self.sensdataH = QLabel(fetch_sens.hum)
+            self.sensorG.addWidget(self.sensdataH, 2, 1)
+
+            self.sensdataA = QLabel(fetch_sens.atp)
+            self.sensorG.addWidget(self.sensdataA, 3, 1)
+
             self.sensgridTemp30 = QLabel(fetch_grid.max30temp)
             self.sensorG.addWidget(self.sensgridTemp30, 1, 2)
+
+            self.sensgridTemp60 = QLabel(fetch_grid.max60temp)
+            self.sensorG.addWidget(self.sensgridTemp60, 1, 3)
+
+            self.sensgridTemp120 = QLabel(fetch_grid.max120temp)
+            self.sensorG.addWidget(self.sensgridTemp120, 1, 4)
+
+            self.sensgridHum30 = QLabel(fetch_grid.max30hum)
+            self.sensorG.addWidget(self.sensgridHum30, 2, 2)
+
+            self.sensgridHum60 = QLabel(fetch_grid.max60hum)
+            self.sensorG.addWidget(self.sensgridHum60, 2, 3)
+
+            self.sensgridHum120 = QLabel(fetch_grid.max120hum)
+            self.sensorG.addWidget(self.sensgridHum120, 2, 4)
+
+            self.sensgridatp30 = QLabel(fetch_grid.max30atp)
+            self.sensorG.addWidget(self.sensgridatp30, 3, 2)
+
+            self.sensgridatp60 = QLabel(fetch_grid.max60atp)
+            self.sensorG.addWidget(self.sensgridatp60, 3, 3)
+
+            self.sensgridatp120 = QLabel(fetch_grid.max120atp)
+            self.sensorG.addWidget(self.sensgridatp120, 3, 4)
+
+            self.sensgridtimestamp = QLabel(fetch_sens.sens_timestamp)
+            self.sensorG.addWidget(self.sensgridtimestamp, 4, 0)
+
+            gps = "Lat: " + fetch_gps.lat + " Lon: " + fetch_gps.long + " Alt: " + fetch_gps.alt + " Time: " + fetch_gps.gps_timestamp
+            self.sensgridgps = QLabel(gps)
+            self.sensorG.addWidget(self.sensgridgps, 5, 0)
+
         except Exception as e:
             print(repr(e))
-
-        self.sensgridTemp60 = QLabel(fetch_grid.max60temp)
-        self.sensorG.addWidget(self.sensgridTemp60, 1, 3)
-
-        self.sensgridTemp120 = QLabel(fetch_grid.max120temp)
-        self.sensorG.addWidget(self.sensgridTemp120, 1, 4)
-        
-        self.sensgridHum30 = QLabel(fetch_grid.max30hum)
-        self.sensorG.addWidget(self.sensgridHum30, 2, 2)
-
-        self.sensgridHum60 = QLabel(fetch_grid.max60hum)
-        self.sensorG.addWidget(self.sensgridHum60, 2, 3)
-
-        self.sensgridHum120 = QLabel(fetch_grid.max120hum)
-        self.sensorG.addWidget(self.sensgridHum120, 2, 4)
-        
-        self.sensgridatp30 = QLabel(fetch_grid.max30atp)
-        self.sensorG.addWidget(self.sensgridatp30, 3, 2)
-
-        self.sensgridatp60 = QLabel(fetch_grid.max60atp)
-        self.sensorG.addWidget(self.sensgridatp60, 3, 3)
-
-        self.sensgridatp120 = QLabel(fetch_grid.max120atp)
-        self.sensorG.addWidget(self.sensgridatp120, 3, 4)
-
-        self.sensgridtimestamp = QLabel(fetch_sens.sens_timestamp)
-        self.sensorG.addWidget(self.sensgridtimestamp, 4, 0)
-
-        gps = "Lat: " + fetch_gps.lat + " Lon: " + fetch_gps.long + " Alt: " + fetch_gps.alt + " Time: " + fetch_gps.gps_timestamp
-        self.sensgridgps = QLabel(gps)
-        self.sensorG.addWidget(self.sensgridgps, 5, 0)
 
         self.sensDataContainer.addLayout(self.sensorG)
         self.windContainer.addWidget(self.sensFrame2)
