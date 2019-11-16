b39a9e612699437f8d2cebe044222fc8761b81eb
fesanb <stefan@bahrawy.net>
2019-11-16 18:12:38 +0100

    WFS V0.4 first steps. Converting to RPI only. Arduino out.

--- wfs-gps.py
+++ wfs-gps.py
@@ -12,7 +12,7 @@ def parseGPS(str):
         print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % (msg.timestamp, msg.lat, msg.lat_dir, msg.lon, msg.lon_dir, msg.altitude, msg.altitude_units))
 
 
-serialPort = serial.Serial("/dev/ttyS0", 9600, timeout=0.5)
+serialPort = serial.Serial("/dev/ttyS0", 9600)
 
 while True:
     str = serialPort.readline()
