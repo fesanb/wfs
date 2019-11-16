f9cf683bb02cbf32324a10197a3dd8046ebb0374
fesanb <stefan@bahrawy.net>
2019-11-16 18:32:10 +0100

    WFS V0.4 first steps. Converting to RPI only. Arduino out.

--- wfs-gps.py
+++ wfs-gps.py
@@ -8,18 +8,19 @@ import serial
 
 def parseGPS(gpsstr):
     if gpsstr.find('GGA') > 0:
-        msg = pynmea2.parse(gpsstr)
-        print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % (msg.timestamp, msg.lat, msg.lat_dir, msg.lon, msg.lon_dir, msg.altitude, msg.altitude_units))
+        try:
+            msg = pynmea2.parse(gpsstr)
+            print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % (msg.timestamp, msg.lat, msg.lat_dir, msg.lon, msg.lon_dir, msg.altitude, msg.altitude_units))
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print(exc_type, exc_tb.tb_lineno)
+            print(repr(e))
 
 
 serialPort = serial.Serial("/dev/ttyS0", 9600)
 
 while True:
-    try:
-        gpsstr = serialPort.readline()
-        print(gpsstr)
-        parseGPS(gpsstr)
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        print(exc_type, exc_tb.tb_lineno)
-        print(repr(e))
+    gpsstr = serialPort.readline()
+    print(gpsstr)
+    parseGPS(gpsstr)
+
