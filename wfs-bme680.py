43d1a8ae885d74d565c9a18c7d0cca48c3ae2e71
fesanb <stefan@bahrawy.net>
2019-11-17 12:35:37 +0100

    WFS V0.4 first steps. Converting to RPI only. Arduino out.

--- wfs-bme680.py
+++ wfs-bme680.py
@@ -3,15 +3,15 @@ import time
 
 sensor = bme680.BME680()
 
-sensor.set_humidity_oversample(bme680.OS_2X)
-sensor.set_pressure_oversample(bme680.OS_4X)
-sensor.set_temperature_oversample(bme680.OS_8X)
-sensor.set_filter(bme680.FILTER_SIZE_3)
-
-sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
-sensor.set_gas_heater_temperature(320)
-sensor.set_gas_heater_duration(150)
-sensor.select_gas_heater_profile(0)
+# sensor.set_humidity_oversample(bme680.OS_2X)
+# sensor.set_pressure_oversample(bme680.OS_4X)
+# sensor.set_temperature_oversample(bme680.OS_8X)
+# sensor.set_filter(bme680.FILTER_SIZE_3)
+#
+# sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+# sensor.set_gas_heater_temperature(320)
+# sensor.set_gas_heater_duration(150)
+# sensor.select_gas_heater_profile(0)
 
 while True:
     if sensor.get_sensor_data():
