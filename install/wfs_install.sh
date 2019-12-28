b8e8590694bcf4bee6f51bdd03a7b1c55e13b842
fesanb <stefan@bahrawy.net>
2019-12-28 18:27:23 +0100

    Error fixing and simplifying.

--- install/wfs_install.sh
+++ install/wfs_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-printf  '\e[93m copying service files to /lib/systemd/system/'
+printf  '\e[93m Copying service files to /lib/systemd/system/'
 
 cp wfs.log.anemo.service /lib/systemd/system/
 cp wfs.log.bme680.service /lib/systemd/system/
@@ -16,4 +16,21 @@ systemctl enable /lib/systemd/system/wfs.service
 
 printf  '\e[92m - Done! \n'
 
+#printf  '\e[93m - Install mariaDB-Server \n'
+#apt install mariadb-server
+#printf  '\e[92m - Done! \n'
+#
+#printf  '\e[93m - Install pip3 \n'
+#apt install pip3
+#printf  '\e[92m - Done! \n'
+#
+#pip3 install bme680
+#pip3 install mysql-connector-python
+#pip3 install numpy
+#pip3 install pynmea2
+#pip3 install pyqt5
+#pip3 install pyqtgraph
+#pip3 install rpi.gpio
+
+printf  '\e[92m - Installation successful \n'
 
