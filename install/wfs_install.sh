551dee2185325d6b995672e30ee947abc338f575
fesanb <stefan@bahrawy.net>
2019-12-28 16:15:42 +0100

    Error fixing and simplifying.

--- install/wfs_install.sh
+++ install/wfs_install.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-echo copying service files to /lib/systemd/system/
+printf  '\e[93m copying service files to /lib/systemd/system/ \n'
 
 cp wfs.log.anemo.service /lib/systemd/system/
 cp wfs.log.bme680.service /lib/systemd/system/
 cp wfs.log.gps.service /lib/systemd/system/
 cp wfs.service /lib/systemd/system/
 
-echo Copying done
-echo Enabling services:
+printf  '\e[92m - Done!'
+printf  '\e[93m Enabling Services \n'
 
 systemctl enable /lib/systemd/system/wfs.log.anemo.service
 systemctl enable /lib/systemd/system/wfs.log.bme680.service
 systemctl enable /lib/systemd/system/wfs.log.gps.service
 systemctl enable /lib/systemd/system/wfs.service
 
-echo Services enabled
-echo install mysql
-apt install mariadb-server
+printf  '\e[92m - Done!'
+
 
