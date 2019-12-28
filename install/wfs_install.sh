2358464fe10b81f6dc293535c724a23663fe7d81
fesanb <stefan@bahrawy.net>
2019-12-28 16:18:59 +0100

    Error fixing and simplifying.

--- install/wfs_install.sh
+++ install/wfs_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-printf  '\e[93m copying service files to /lib/systemd/system/ \n'
+printf  '\e[93m copying service files to /lib/systemd/system/ \r'
 
 cp wfs.log.anemo.service /lib/systemd/system/
 cp wfs.log.bme680.service /lib/systemd/system/
@@ -7,7 +7,7 @@ cp wfs.log.gps.service /lib/systemd/system/
 cp wfs.service /lib/systemd/system/
 
 printf  '\e[92m - Done!'
-printf  '\e[93m Enabling Services \n'
+printf  '\e[93m Enabling Services \r'
 
 systemctl enable /lib/systemd/system/wfs.log.anemo.service
 systemctl enable /lib/systemd/system/wfs.log.bme680.service
