d4a35d94c212764ad8208cfbc1ef5b18bc91e09a
fesan <Kukskalle231086>
2020-10-11 16:06:07 +0200

    Trying to reduce memory usage.

--- wfs-gps.py
+++ wfs-gps.py
@@ -35,54 +35,59 @@ def db_insert(lat, lon, alt, sat):
 	cnx.close()
 
 
-ser = serial.Serial("/dev/ttyAMA0", 9600, timeout=1)
+try:
+	ser = serial.Serial("/dev/ttyAMA0", 9600, timeout=1)
 
 
-def parse_gps(gps_sig):
-	if gps_sig.find('GGA') > 0:
-		msg = pynmea2.parse(gps_sig)
-		if len(msg.lat) == 0:
-			parse_gps.sig = False
-			pass
+	def parse_gps(gps_sig):
+		if gps_sig.find('GGA') > 0:
+			msg = pynmea2.parse(gps_sig)
+			if len(msg.lat) == 0:
+				parse_gps.sig = False
+				pass
+			else:
+				parse_gps.sig = True
+				parse_gps.lat = msg.latitude
+				parse_gps.lon = msg.longitude
+				parse_gps.alt = msg.altitude
+				parse_gps.sat = msg.num_sats
 		else:
-			parse_gps.sig = True
-			parse_gps.lat = msg.latitude
-			parse_gps.lon = msg.longitude
-			parse_gps.alt = msg.altitude
-			parse_gps.sat = msg.num_sats
-	else:
-		parse_gps.sig = False
+			parse_gps.sig = False
 
 
-gps_iter = 0
-gps_list = []
+	gps_iter = 0
+	gps_list = []
 
-while gps_iter < 20:
-	gps = ser.readline().decode()
-	parse_gps(gps)  # random.choice([fake_sig, fake_sig1, fake_sig2, fake_sig3])
-	if parse_gps.sig is True:
-		gps_list.append([parse_gps.lat, parse_gps.lon, parse_gps.alt, parse_gps.sat])
-	gps_iter = gps_iter + 1
+	while gps_iter < 20:
+		gps = ser.readline().decode()
+		parse_gps(gps)  # random.choice([fake_sig, fake_sig1, fake_sig2, fake_sig3])
+		if parse_gps.sig is True:
+			gps_list.append([parse_gps.lat, parse_gps.lon, parse_gps.alt, parse_gps.sat])
+		gps_iter = gps_iter + 1
 
-c = 0
-c2 = 0
-equal = 0
-equal_list = []
-
-while c < len(gps_list):
-	for i in gps_list:
-		if gps_list[c] == gps_list[c2]:
-			equal = equal + 1
-		c2 = c2 + 1
-	equal_list.append(equal)
-	equal = 0
+	c = 0
 	c2 = 0
-	c = c + 1
+	equal = 0
+	equal_list = []
 
-m = max(equal_list)
-equals = equal_list.count(max(equal_list))
-index = [i for i, j in enumerate(equal_list) if j == m]
+	while c < len(gps_list):
+		for i in gps_list:
+			if gps_list[c] == gps_list[c2]:
+				equal = equal + 1
+			c2 = c2 + 1
+		equal_list.append(equal)
+		equal = 0
+		c2 = 0
+		c = c + 1
 
-if equals > 5:
-	insert = gps_list[index[0]]
-	db_insert(insert[0], insert[1], insert[2], insert[3])
+	m = max(equal_list)
+	equals = equal_list.count(max(equal_list))
+	index = [i for i, j in enumerate(equal_list) if j == m]
+
+	if equals > 5:
+		insert = gps_list[index[0]]
+		db_insert(insert[0], insert[1], insert[2], insert[3])
+
+except Exception as e:
+		filename = Path(__file__).name
+		error_handle(e, filename)
