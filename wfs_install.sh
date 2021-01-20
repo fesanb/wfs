9b9baa8badcedbd61a1dfd31746c4b9a4b5de23b
fesan <Kukskalle231086>
2021-01-20 21:09:29 +0100

    Fixing small issues.

--- wfs_install.sh
+++ wfs_install.sh
@@ -4,16 +4,16 @@ echo "disable_splash=1" | sudo tee -a /boot/config.txt # Disable Bluetooth
 systemctl disable hciuart # Free up UART to allow serial communication with GPS.
 
 printf  '\e[93m Copying service files to /lib/systemd/system/ \e[0m \n'
-cp /service-timer/wfs.mean.service /lib/systemd/system/
-cp /service-timer/wfs.mean.timer /lib/systemd/system/
-cp /service-timer/wfs.log.anemo.service /lib/systemd/system/
-cp /service-timer/wfs.log.bme680.service /lib/systemd/system/
-cp /service-timer/wfs.log.bme680.timer /lib/systemd/system/
-cp /service-timer/wfs.log.gps.service /lib/systemd/system/
-cp /service-timer/wfs.log.gps.timer /lib/systemd/system/
-cp /service-timer/wfs.service /lib/systemd/system/
-cp /service-timer/wfs.clean.service /lib/systemd/system/
-cp /service-timer/wfs.clean.timer /lib/systemd/system/
+cp service-timer/wfs.mean.service /lib/systemd/system/
+cp service-timer/wfs.mean.timer /lib/systemd/system/
+cp service-timer/wfs.log.anemo.service /lib/systemd/system/
+cp service-timer/wfs.log.bme680.service /lib/systemd/system/
+cp service-timer/wfs.log.bme680.timer /lib/systemd/system/
+cp service-timer/wfs.log.gps.service /lib/systemd/system/
+cp service-timer/wfs.log.gps.timer /lib/systemd/system/
+cp service-timer/wfs.service /lib/systemd/system/
+cp service-timer/wfs.clean.service /lib/systemd/system/
+cp service-timer/wfs.clean.timer /lib/systemd/system/
 printf  '\e[92m - Done! \e[0m \n'
 
 printf  '\e[93m Setting a new splash image \e[0m \n' # Better way is to change plymouth theme with custom one.
--- wfs_install_small.sh
+++ wfs_install_small.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 printf  '\e[93m Copying service files to /lib/systemd/system/ \e[0m \n'
-cp /service-timer/wfs.mean.service /lib/systemd/system/
-cp /service-timer/wfs.mean.timer /lib/systemd/system/
-cp /service-timer/wfs.log.anemo.service /lib/systemd/system/
-cp /service-timer/wfs.log.bme680.service /lib/systemd/system/
-cp /service-timer/wfs.log.bme680.timer /lib/systemd/system/
-cp /service-timer/wfs.log.gps.service /lib/systemd/system/
-cp /service-timer/wfs.log.gps.timer /lib/systemd/system/
-cp /service-timer/wfs.service /lib/systemd/system/
-cp /service-timer/wfs.clean.service /lib/systemd/system/
-cp /service-timer/wfs.clean.timer /lib/systemd/system/
+cp service-timer/wfs.mean.service /lib/systemd/system/
+cp service-timer/wfs.mean.timer /lib/systemd/system/
+cp service-timer/wfs.log.anemo.service /lib/systemd/system/
+cp service-timer/wfs.log.bme680.service /lib/systemd/system/
+cp service-timer/wfs.log.bme680.timer /lib/systemd/system/
+cp service-timer/wfs.log.gps.service /lib/systemd/system/
+cp service-timer/wfs.log.gps.timer /lib/systemd/system/
+cp service-timer/wfs.service /lib/systemd/system/
+cp service-timer/wfs.clean.service /lib/systemd/system/
+cp service-timer/wfs.clean.timer /lib/systemd/system/
 printf  '\e[92m - Done! \e[0m \n'
 
 printf  '\e[93m Setting a new splash image \e[0m \n' # Better way is to change plymouth theme with custom one.
